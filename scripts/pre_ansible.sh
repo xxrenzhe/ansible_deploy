96d4f77a37402568b1a7b37633d0ef691872ee25
xxrenzhe <yj2008ay611@gmail.com>
2014-07-16 19:17:55 +0800

    Update pre_ansible.sh
    insert "ssh public key" into server's  authorized_keys

--- scripts/pre_ansible.sh
+++ scripts/pre_ansible.sh
@@ -17,4 +17,5 @@ sed -i 's/^#\s\+\(%wheel\s\+ALL=(ALL)\s\+NOPASSWD:\s\+ALL\)/\1/p' /etc/sudoers
 ###### Deploy Control SSH Public Key into wenba's authorized_keys ######
 Ipaddr=`ifconfig |grep -A 1 eth0|grep "inet addr"|awk '{print $2}'|awk -F':' '{print $2}'`
 ssh wenba@$Ipaddr 'ssh-keygen'
-echo "xxoo" >/home/wenba/.ssh/authorized_keys
+echo "${control ssh public key}" > /home/wenba/.ssh/authorized_keys
+echo "${ansible control server ssh public key}" >> /home/wenba/.ssh/authorized_keys
